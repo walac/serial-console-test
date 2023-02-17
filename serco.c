44b4474ec6a942b0efb96bc9aa4037a175309273
Wander Lairson Costa <wander@redhat.com>
2023-02-17 12:33:17 -0300

    format source code for kernel coding-style

--- serco.c
+++ serco.c
@@ -12,45 +12,45 @@
 static struct console *cons;
 
 static ssize_t serco_write(struct file *filp, const char __user *buf,
-        size_t count, loff_t *pos)
+			   size_t count, loff_t *pos)
 {
-    char *dest;
+	char *dest;
 
-    dest = kmalloc(count+1, GFP_KERNEL);
-    if (!dest) {
-        pr_err("No memory available to allocate %zu bytes\n", count);
-        return -ENOMEM;
-    }
+	dest = kmalloc(count+1, GFP_KERNEL);
+	if (!dest) {
+		pr_err("No memory available to allocate %zu bytes\n", count);
+		return -ENOMEM;
+	}
 
-    if (copy_from_user(dest, buf, count)) {
-        kfree(dest);
-        pr_err("copy_from_user cannot copy all bytes\n");
-        return -EPERM;
-    }
+	if (copy_from_user(dest, buf, count)) {
+		kfree(dest);
+		pr_err("copy_from_user cannot copy all bytes\n");
+		return -EPERM;
+	}
 
-    if (cons) {
-        cons->write(cons, dest, count);
-    } else {
-        dest[count] = '\0';
-        pr_info("%s\n", dest);
-    }
+	if (cons) {
+		cons->write(cons, dest, count);
+	} else {
+		dest[count] = '\0';
+		pr_info("%s\n", dest);
+	}
 
-    kfree(dest);
+	kfree(dest);
 
-    *pos += count;
-    return count;
+	*pos += count;
+	return count;
 }
 
 static int serco_open(struct inode *inode, struct file *filp)
 {
-    return nonseekable_open(inode, filp);
+	return nonseekable_open(inode, filp);
 }
 
 static const struct file_operations serco_fops = {
-    .owner = THIS_MODULE,
-    .write = serco_write,
-    .llseek = no_llseek,
-    .open = serco_open,
+	.owner = THIS_MODULE,
+	.write = serco_write,
+	.llseek = no_llseek,
+	.open = serco_open,
 };
 
 static struct miscdevice serco_miscdev = {
@@ -62,15 +62,15 @@ static struct miscdevice serco_miscdev = {
 
 static int __init serco_init(void)
 {
-    /* look for the serial console driver */
-    for_each_console(cons)
-        if (strstarts(cons->name, "ttyS"))
-            break;
+	/* look for the serial console driver */
+	for_each_console(cons)
+		if (strstarts(cons->name, "ttyS"))
+			break;
 
-    if (!cons)
-        pr_warn("Could not find a serial console\n");
+	if (!cons)
+		pr_warn("Could not find a serial console\n");
 
-    return misc_register(&serco_miscdev);
+	return misc_register(&serco_miscdev);
 }
 
 static void __exit serco_exit(void)
--- sertest.c
+++ sertest.c
@@ -11,41 +11,41 @@ static const int data_size = sizeof(data) - sizeof(data[0]);
 
 int main(int argc, char **argv)
 {
-    int opt, i, k, n = 0;
-    char *buf;
-    int fd;
+	int opt, i, k, n = 0;
+	char *buf;
+	int fd;
 
-    while ((opt = getopt(argc, argv, "n:")) != -1) {
-        switch (opt) {
-            case 'n':
-                n = atoi(optarg);
-                k = n / data_size;
-                break;
-        }
-    }
+	while ((opt = getopt(argc, argv, "n:")) != -1) {
+		switch (opt) {
+		case 'n':
+			n = atoi(optarg);
+			k = n / data_size;
+			break;
+		}
+	}
 
-    if (n <= 0)
-        return EXIT_FAILURE;
+	if (n <= 0)
+		return EXIT_FAILURE;
 
-    buf = calloc(n, sizeof(char));
-    if (!buf)
-        return EXIT_FAILURE;
+	buf = calloc(n, sizeof(char));
+	if (!buf)
+		return EXIT_FAILURE;
 
-    for (i = 0; i < k; ++i)
-        memcpy(buf + i * data_size, data, data_size);
+	for (i = 0; i < k; ++i)
+		memcpy(buf + i * data_size, data, data_size);
 
-    memcpy(buf + i * data_size, data, n % data_size);
+	memcpy(buf + i * data_size, data, n % data_size);
 
-    fd = open(argv[optind], O_WRONLY);
-    if (fd < 0) {
-        perror("open");
-        return EXIT_FAILURE;
-    }
+	fd = open(argv[optind], O_WRONLY);
+	if (fd < 0) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
 
-    if (write(fd, buf, n) < 0)
-        perror("write");
+	if (write(fd, buf, n) < 0)
+		perror("write");
 
-    close(fd);
+	close(fd);
 
-    return 0;
+	return 0;
 }
